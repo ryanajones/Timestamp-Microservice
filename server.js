55453186ca5a519b2abe38c1b5ac9be183ae3812
Ryan Jones <ryanajonesdev@gmail.com>
2020-09-05 21:12:14 -0700

    adjusted port

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get("/api/timestamp/:time?", (req, res) => {
   } 
 })
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT || 5000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
